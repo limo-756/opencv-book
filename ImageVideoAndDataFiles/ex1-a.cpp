cd547a8fcf9ebe55312c8e11c997d2eacaad3282
anurag.sh <anuragsh@flock.com>
2021-07-01 09:31:55 +0530

    [feature] Show src video, gray scale video and canny edge detection video in same window

---
+++ ImageVideoAndDataFiles/ex1-a.cpp
@@ -0,0 +1,39 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/mat.hpp>
+#include "../FixedSizeMultiImageHandler.h"
+using namespace std;
+using namespace cv;
+
+const string WINDOW_NAME = "SHOW_CANNY_EDGE_PROCESS";
+
+int main() {
+    namedWindow(WINDOW_NAME, WINDOW_NORMAL);
+    moveWindow(WINDOW_NAME, 300, 300);
+
+    VideoCapture videoCapture = VideoCapture("/Users/anurag.sh/Season2/naruto/9.mp4");
+
+    if (!videoCapture.isOpened()) {
+        cout << "File could not be opened" << endl;
+        return -1;
+    }
+
+    Mat frame;
+    while (true) {
+        videoCapture >> frame;
+        if (frame.empty()) {
+            cout << "Video ended" << endl;
+            break;
+        }
+
+        Mat grayScaleImage, gaussianBlurImage, cannyEdgeDetection;
+        cvtColor(frame, grayScaleImage, COLOR_BGR2GRAY, 1);
+        GaussianBlur(grayScaleImage, gaussianBlurImage, Size(5,5), 0.0);
+        Canny(gaussianBlurImage, cannyEdgeDetection, 10, 30);
+        vector<Mat> imgVec({frame, grayScaleImage, gaussianBlurImage, cannyEdgeDetection});
+        FixedSizeMultiImageHandler imageHandler = FixedSizeMultiImageHandler(imgVec, Size(600,600));
+        imshow(WINDOW_NAME, imageHandler.getCollatedImage());
+        if (waitKey(10) == 27 ) break;
+    }
+
+    return 0;
+}
